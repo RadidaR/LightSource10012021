34d130a2a9ac4eb18e68b94e6035cf2991c56ffe
andrea-jovanovic <andrea.jovanovic@gmail.com>
2021-02-28 23:16:36 +0000

    light

--- Unity/LightSource10012021/Assets/Player/Data/Lantern/LanternLightScript.cs
+++ Unity/LightSource10012021/Assets/Player/Data/Lantern/LanternLightScript.cs
@@ -37,16 +37,211 @@ public class LanternLightScript : MonoBehaviour
         lightRange.radius = lightSource.pointLightOuterRadius;
         lightSource.pointLightOuterRadius = playerStaminaData.currentStamina;
         lightSource.pointLightInnerRadius = playerStaminaData.currentStamina / 10;
-        RaycastHit2D threeRay = Physics2D.Raycast(gameObject.transform.position, pos3.position - gameObject.transform.position, lightSource.pointLightOuterRadius, groundLayer);
-        if (!threeRay)
+        
+        
+        RaycastHit2D ray1 = Physics2D.Raycast(gameObject.transform.position, pos1.position - gameObject.transform.position, lightSource.pointLightOuterRadius, groundLayer);
+        RaycastHit2D ray2 = Physics2D.Raycast(gameObject.transform.position, pos2.position - gameObject.transform.position, lightSource.pointLightOuterRadius, groundLayer);
+        RaycastHit2D ray3 = Physics2D.Raycast(gameObject.transform.position, pos3.position - gameObject.transform.position, lightSource.pointLightOuterRadius, groundLayer);
+        RaycastHit2D ray4 = Physics2D.Raycast(gameObject.transform.position, pos4.position - gameObject.transform.position, lightSource.pointLightOuterRadius, groundLayer);
+        RaycastHit2D ray5 = Physics2D.Raycast(gameObject.transform.position, pos5.position - gameObject.transform.position, lightSource.pointLightOuterRadius, groundLayer);
+        RaycastHit2D ray6 = Physics2D.Raycast(gameObject.transform.position, pos6.position - gameObject.transform.position, lightSource.pointLightOuterRadius, groundLayer);
+        RaycastHit2D ray7 = Physics2D.Raycast(gameObject.transform.position, pos7.position - gameObject.transform.position, lightSource.pointLightOuterRadius, groundLayer);
+        RaycastHit2D ray8 = Physics2D.Raycast(gameObject.transform.position, pos8.position - gameObject.transform.position, lightSource.pointLightOuterRadius, groundLayer);
+        RaycastHit2D ray9 = Physics2D.Raycast(gameObject.transform.position, pos9.position - gameObject.transform.position, lightSource.pointLightOuterRadius, groundLayer);
+        RaycastHit2D ray10 = Physics2D.Raycast(gameObject.transform.position, pos10.position - gameObject.transform.position, lightSource.pointLightOuterRadius, groundLayer);
+        RaycastHit2D ray11 = Physics2D.Raycast(gameObject.transform.position, pos11.position - gameObject.transform.position, lightSource.pointLightOuterRadius, groundLayer);
+        RaycastHit2D ray12 = Physics2D.Raycast(gameObject.transform.position, pos12.position - gameObject.transform.position, lightSource.pointLightOuterRadius, groundLayer);
+
+        if (!ray1)
         {
-            Debug.Log("three ray clear");
+            pos1.position = gameObject.transform.position + (pos1.position - gameObject.transform.position).normalized * lightSource.pointLightOuterRadius;
+        }
+        else
+        {
+            if (ray1.distance > 0)
+            {
+                pos1.position = gameObject.transform.position + (pos1.position - gameObject.transform.position).normalized * ray1.distance;
+            }
+            else
+            {
+                pos1.position = gameObject.transform.position + (pos1.position - gameObject.transform.position).normalized;
+            }    
+        }
+
+        if (!ray2)
+        {
+            pos2.position = gameObject.transform.position + (pos2.position - gameObject.transform.position).normalized * lightSource.pointLightOuterRadius;
+        }
+        else
+        {
+            if (ray2.distance > 0)
+            {
+                pos2.position = gameObject.transform.position + (pos2.position - gameObject.transform.position).normalized * ray2.distance;
+            }
+            else
+            {
+                pos2.position = gameObject.transform.position + (pos2.position - gameObject.transform.position).normalized;
+            }
+        }
+
+        if (!ray3)
+        {
             pos3.position = gameObject.transform.position + (pos3.position - gameObject.transform.position).normalized * lightSource.pointLightOuterRadius;
         }
         else
         {
-            Debug.Log("hit");
-            pos3.position = gameObject.transform.position + (pos3.position - gameObject.transform.position).normalized * threeRay.distance;
+            if (ray3.distance > 0)
+            {
+                pos3.position = gameObject.transform.position + (pos3.position - gameObject.transform.position).normalized * ray3.distance;
+            }
+            else
+            {
+                pos3.position = gameObject.transform.position + (pos3.position - gameObject.transform.position).normalized;
+            }
+        }
+
+        if (!ray4)
+        {
+            pos4.position = gameObject.transform.position + (pos4.position - gameObject.transform.position).normalized * lightSource.pointLightOuterRadius;
+        }
+        else
+        {
+            if (ray4.distance > 0)
+            {
+                pos4.position = gameObject.transform.position + (pos4.position - gameObject.transform.position).normalized * ray4.distance;
+            }
+            else
+            {
+                pos4.position = gameObject.transform.position + (pos4.position - gameObject.transform.position).normalized;
+            }
+        }
+
+        if (!ray5)
+        {
+            pos5.position = gameObject.transform.position + (pos5.position - gameObject.transform.position).normalized * lightSource.pointLightOuterRadius;
+        }
+        else
+        {
+            if (ray5.distance > 0)
+            {
+                pos5.position = gameObject.transform.position + (pos5.position - gameObject.transform.position).normalized * ray5.distance;
+            }
+            else
+            {
+                pos5.position = gameObject.transform.position + (pos5.position - gameObject.transform.position).normalized;
+            }
+        }
+
+        if (!ray6)
+        {
+            pos6.position = gameObject.transform.position + (pos6.position - gameObject.transform.position).normalized * lightSource.pointLightOuterRadius;
+        }
+        else
+        {
+            if (ray6.distance > 0)
+            {
+                pos6.position = gameObject.transform.position + (pos6.position - gameObject.transform.position).normalized * ray6.distance;
+            }
+            else
+            {
+                pos6.position = gameObject.transform.position + (pos6.position - gameObject.transform.position).normalized;
+            }
+        }
+
+        if (!ray7)
+        {
+            pos7.position = gameObject.transform.position + (pos7.position - gameObject.transform.position).normalized * lightSource.pointLightOuterRadius;
+        }
+        else
+        {
+            if (ray7.distance > 0)
+            {
+                pos7.position = gameObject.transform.position + (pos7.position - gameObject.transform.position).normalized * ray7.distance;
+            }
+            else
+            {
+                pos7.position = gameObject.transform.position + (pos7.position - gameObject.transform.position).normalized;
+            }
+        }
+
+        if (!ray8)
+        {
+            pos8.position = gameObject.transform.position + (pos8.position - gameObject.transform.position).normalized * lightSource.pointLightOuterRadius;
+        }
+        else
+        {
+            if (ray8.distance > 0)
+            {
+                pos8.position = gameObject.transform.position + (pos8.position - gameObject.transform.position).normalized * ray8.distance;
+            }
+            else
+            {
+                pos8.position = gameObject.transform.position + (pos8.position - gameObject.transform.position).normalized;
+            }
+        }
+
+        if (!ray9)
+        {
+            pos9.position = gameObject.transform.position + (pos9.position - gameObject.transform.position).normalized * lightSource.pointLightOuterRadius;
+        }
+        else
+        {
+            if (ray9.distance > 0)
+            {
+                pos9.position = gameObject.transform.position + (pos9.position - gameObject.transform.position).normalized * ray9.distance;
+            }
+            else
+            {
+                pos9.position = gameObject.transform.position + (pos9.position - gameObject.transform.position).normalized;
+            }
+        }
+
+        if (!ray10)
+        {
+            pos10.position = gameObject.transform.position + (pos10.position - gameObject.transform.position).normalized * lightSource.pointLightOuterRadius;
+        }
+        else
+        {
+            if (ray10.distance > 0)
+            {
+                pos10.position = gameObject.transform.position + (pos10.position - gameObject.transform.position).normalized * ray10.distance;
+            }
+            else
+            {
+                pos10.position = gameObject.transform.position + (pos10.position - gameObject.transform.position).normalized;
+            }
+        }
+
+        if (!ray11)
+        {
+            pos11.position = gameObject.transform.position + (pos11.position - gameObject.transform.position).normalized * lightSource.pointLightOuterRadius;
+        }
+        else
+        {
+            if (ray11.distance > 0)
+            {
+                pos11.position = gameObject.transform.position + (pos11.position - gameObject.transform.position).normalized * ray11.distance;
+            }
+            else
+            {
+                pos11.position = gameObject.transform.position + (pos11.position - gameObject.transform.position).normalized;
+            }
+        }
+
+        if (!ray12)
+        {
+            pos12.position = gameObject.transform.position + (pos12.position - gameObject.transform.position).normalized * lightSource.pointLightOuterRadius;
+        }
+        else
+        {
+            if (ray12.distance > 0)
+            {
+                pos12.position = gameObject.transform.position + (pos12.position - gameObject.transform.position).normalized * ray12.distance;
+            }
+            else
+            {
+                pos12.position = gameObject.transform.position + (pos12.position - gameObject.transform.position).normalized;
+            }
         }
     }
 
